51bb771fe1932b1198cf45db5c6250a9e8236f4a
Binary Birch Tree <binarybirchtree@users.noreply.github.com>
2016-08-17 21:17:16 -0400

    Updated Travis CI configuration.

--- contributionart.sh
+++ contributionart.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 echo "Running Gradle."
-gradle --daemon build installDist
+gradle installDist
 
 echo "Running ContributionArt."
 build/install/contributionart/bin/contributionart \
