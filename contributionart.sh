dfb3b838233a300853a17401c37e5bf00a70dfad
Binary Birch Tree <binarybirchtree@users.noreply.github.com>
2016-08-17 20:00:57 -0400

    Added GPLv3 license information.

--- contributionart.sh
+++ contributionart.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -e
 
+# This file is part of ContributionArt.
+# Copyright (C) 2015, 2016 Binary Birch Tree
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 echo "Running Gradle."
 gradle --daemon build installDist
 
@@ -10,4 +26,3 @@ build/install/contributionart/bin/contributionart \
 --name "$(git config user.name)" \
 --email "$(git config user.email)" \
 --factor 20
-
